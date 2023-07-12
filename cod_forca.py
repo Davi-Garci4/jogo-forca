bbc9fd5a0958e55b7a9c946a0b1dae227d3a2b8c
Davi-Garci4 <davi.garcia@aluno.faculdadeimpacta.com.br>
2023-07-12 15:32:51 -0300

    adicionei uma compressão de lista na hora de se trazer a quantidade de letras da palavra secreta

--- cod_forca.py
+++ cod_forca.py
@@ -6,7 +6,7 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_",]
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
@@ -35,5 +35,5 @@ def jogar():
 
     print("Fim de jogo...")
 
-    if (__name__ == "__main__"):
+if (__name__ == "__main__"):
         jogar()
